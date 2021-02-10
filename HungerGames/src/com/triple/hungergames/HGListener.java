6ffd8e0d6dba0f10df186c8ee666f410855da5ba
Triple1996 <unconfigured@null.spigotmc.org>
2021-02-10 01:50:44 -0500

    Add HGListener Class, /welcome command, and remove unnecessary files

---
+++ HungerGames/src/com/triple/hungergames/HGListener.java
@@ -0,0 +1,43 @@
+package com.triple.hungergames;
+
+import java.lang.Math;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class HGListener implements Listener{
+
+    // Constructor
+    public HGListener(Main plugin) {
+                
+    }
+    
+    @EventHandler
+    public void OnPlayerJoin(PlayerJoinEvent event) {
+        
+        Player player = (Player) event.getPlayer();
+        
+        player.sendMessage("Welcome to the server " + player.getName());
+
+    }    
+    
+    @EventHandler
+    public void onPlayerDamage(PlayerMoveEvent event) {
+        
+        Location location = event.getPlayer().getLocation();
+                
+        if ( (Math.abs( location.getX() ) > 16) || (Math.abs( location.getZ() ) > 16) ) {
+        	
+        	Location spawn = new Location(Bukkit.getServer().getWorld("world"), 0, 62, 0);
+        	event.getPlayer().teleport(spawn);
+        	// TODO Remove hard-coded y=62. This code is for demonstration purposes only
+        	
+        }
+        
+    }
+}
--- HungerGames/src/com/triple/hungergames/Main.java
+++ HungerGames/src/com/triple/hungergames/Main.java
@@ -1,18 +1,47 @@
 package com.triple.hungergames;
-
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin{
+    
+    @Override
+    public void onEnable() {
+        getLogger().info("UHC has been enabled.");
+        PluginManager pm = getServer().getPluginManager();
+        HGListener listener = new HGListener(this);
+        pm.registerEvents(listener, this);
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("UHC has been disabled.");
+    }    
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        
+        Player player = (Player) sender;
+        
+        if (sender instanceof Player) {
+            
+            String lowerCmd = cmd.getName().toLowerCase();
 
-	@Override
-	public void onEnable() {
-		
-		getLogger().info("HungerGames has been enabled.");
-	}
-	
-	@Override
-	public void onDisable() {
-		
-		getLogger().info("HungerGames has been disabled.");
-	}
+            switch (lowerCmd) {
+            
+            case "welcome":
+            	player.sendMessage("Welcome " + player.getName());
+            	return true;
+            
+            default:
+                
+                player.sendMessage("Your command was not recognized.");
+                return true;
+            }
+        }
+        player.sendMessage("Somehow you aren't a player.");
+        return true;
+    }
+    
 }
