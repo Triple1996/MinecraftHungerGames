8040616b3595e6ad7b7444e9b4e3e5125fb631e8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-09 20:09:14 -0500

    Initial Commit with boilerplate. onEnable() and onDisable() functions overrided

---
+++ HungerGames/src/com/triple/hungergames/Main.java
@@ -0,0 +1,18 @@
+package com.triple.hungergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+		getLogger().info("HungerGames has been enabled.");
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		getLogger().info("HungerGames has been disabled.");
+	}
+}
