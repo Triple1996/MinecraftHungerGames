0f0b592dafc4efbdabd75c38cac0e147cf462a57
triple <unconfigured@null.spigotmc.org>
2021-02-11 12:14:42 -0500

    Add onWorldInit listener, initializes world border, spawnpoint

--- HungerGames/src/com/triple/hungergames/HGListener.java
+++ HungerGames/src/com/triple/hungergames/HGListener.java
@@ -4,7 +4,10 @@ import java.lang.Math;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.WorldBorder;
+import org.bukkit.block.Block;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,20 +15,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.event.world.WorldInitEvent;
+
 
 public class HGListener implements Listener{
 
+	private HGHelper hgh = new HGHelper();
+	private ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+	private World world = Bukkit.getWorld("world");
+	private WorldBorder border = world.getWorldBorder();
+
+	final static int MAX_BUILD_HEIGHT = 256;
+	
     // Constructor
     public HGListener(Main plugin) {
 
     }
-
+    
+    
+    
     @EventHandler
     public void OnPlayerJoin(PlayerJoinEvent event) {
 
         Player player = (Player) event.getPlayer();
-
         player.sendMessage("Welcome to the server " + player.getName());
+        // TODO Initialize player's death count and kills to 0
 
     }
 
@@ -33,7 +47,6 @@ public class HGListener implements Listener{
     public void onPlayerMove(PlayerMoveEvent event) {
 
         Location location = event.getPlayer().getLocation();
-        HGHelper hgh = new HGHelper();
         if ( (Math.abs( location.getX() ) > 16) || (Math.abs( location.getZ() ) > 16) ) {
         	Location spawn = new Location(Bukkit.getWorld("world"), 0, hgh.getYValOfSurface(0,0), 0);
         	event.getPlayer().teleport(spawn);
@@ -42,15 +55,34 @@ public class HGListener implements Listener{
     
     @EventHandler
     public void onLoad(ServerLoadEvent event) {
-
-    	WorldBorder border = Bukkit.getWorld("world").getWorldBorder();
-    	ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+    	
     	border.setCenter(0, 0);
     	border.setSize(150);
-
+    	border.setWarningDistance(0);
     	Bukkit.dispatchCommand(console, "spreadplayers 0 0 0 1 true @a");
 
     	
+    }
+    
+    @EventHandler
+    public void onWorldInit(WorldInitEvent event) {
+    	// TODO initteams, initscoreboard, initdeathcount, initspawnplatform
+    	// TODO make sure this actually only loads once when the world is first genned
+    	
+    	border.setCenter(0, 0);
+    	world.setSpawnLocation(0, (int)world.getSpawnLocation().getY(), 0);
+    	border.setCenter(0, 0);
+    	border.setSize(150);
+    	border.setWarningDistance(0);
+    	hgh.tellConsole(console, "initialized world border");
+    	
+    	
+    	Block pillarOrigin = new Location(world, 0,3,0).getBlock();
+    	for (int i = 3; i < MAX_BUILD_HEIGHT; i++) {
+    		if (!(pillarOrigin.getType().equals(Material.AIR))) {
+    			pillarOrigin.setType(Material.GLASS);
+    		}
+    	}
     }
 }
 
