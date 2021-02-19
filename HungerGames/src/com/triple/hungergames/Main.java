8ea987d399a7aa11947e13cdade1802d0be7bfbd
triple <45209813+Triple1996@users.noreply.github.com>
2021-02-19 11:43:02 -0500

    Create list of potion effects, give to player on join and reload

--- HungerGames/src/com/triple/hungergames/Main.java
+++ HungerGames/src/com/triple/hungergames/Main.java
@@ -1,19 +1,29 @@
 package com.triple.hungergames;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Main extends JavaPlugin{
 	
+	public static List<PotionEffect> starterEffects = new ArrayList<PotionEffect>();
+	public final static int MAX_BUILD_HEIGHT = 256;
     @Override
     public void onEnable() {
     	this.getConfig().addDefault("teamsInit", false);
     	this.getConfig().addDefault("scoreBoardInit", false);
     	this.getConfig().addDefault("deathCountInit", false);
     	
+    	starterEffects.add(new PotionEffect(PotionEffectType.REGENERATION, 10000, 10));
+        starterEffects.add(new PotionEffect(PotionEffectType.SATURATION, 10000, 10));
+        
         getLogger().info("HungerGames has been enabled.");
         PluginManager pm = getServer().getPluginManager();
         HGListener listener = new HGListener(this);
