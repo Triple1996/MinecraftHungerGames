4102d62905092ec4b4cf551bcc84b40048567587
triple <45209813+Triple1996@users.noreply.github.com>
2021-02-19 15:34:21 -0500

    On start command set diffculty hard, gamemode survival, and give everyone a compass

--- HungerGames/src/com/triple/hungergames/Main.java
+++ HungerGames/src/com/triple/hungergames/Main.java
@@ -5,15 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class Main extends JavaPlugin{
 
@@ -54,23 +59,35 @@ public class Main extends JavaPlugin{
             
             case "start":
             	// TODO set world border to shrink, set up scheduler
-            	// TODO difficulty hard, give compass, time set day, gamemode survival
-            	// TODO reset kills and deaths
             	ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+            	Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
             	World world = Bukkit.getServer().getWorld("world");
             	Player player;
 	
             	world.getWorldBorder().setCenter(0, 0);
             	world.getWorldBorder().setSize(2000);
             	world.getWorldBorder().setWarningDistance(100);
-            	            	
-            	Bukkit.dispatchCommand(console, "spreadplayers 0 0 300 900 true @a");
+            	
+            	
+                world.setDifficulty(Difficulty.HARD);
+                world.setTime(1000);
+                
+            	ItemStack compass = new ItemStack(Material.COMPASS, 1);
+            	
+            	// "for each player"
             	List<Player> players = world.getPlayers();
             	for (int i = 0; i < players.size(); i++) {
             		player = players.get(i);
+            		scoreboard.getObjective("kills").getScore(player.getName()).setScore(0); 
+                    scoreboard.getObjective("deaths").getScore(player.getName()).setScore(0); 
+            		player.setGameMode(GameMode.SURVIVAL);
             		hgu.clearEffects(player);
             		player.getInventory().clear();
+            		player.getInventory().setItem(8, compass);
             	}
+            	
+            	Bukkit.dispatchCommand(console, "spreadplayers 0 0 300 900 true @a");
+            	
             	return true;
             	
             case "welcome":
